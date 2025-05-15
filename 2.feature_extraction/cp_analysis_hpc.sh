55da4c705008cc7984af11e83e27da042eb368cd
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2025-05-15 09:31:05 -0600

    small fix per PR review

--- 2.feature_extraction/cp_analysis_hpc.sh
+++ 2.feature_extraction/cp_analysis_hpc.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=amilan
 #SBATCH --qos=long
 #SBATCH --account=amc-general
-#SBATCH --time=5-00:00 (days-hours:min:sec) # estimate time for one plate testing
+#SBATCH --time=5-00:00:00 (days-hours:min:sec) # estimate time for one plate testing
 #SBATCH --output=run_CP-%j.out
 
 module load miniforge
