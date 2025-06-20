5abe0954054c6fd605a93b3d0bc7f2ac73de7f89
Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
2025-06-20 10:53:57 -0600

    Process Round 2 data (#26)
    * process Round 2 data
    
    * add single cell processing
    
    * fix outlines bug

--- 2.feature_extraction/cp_analysis_hpc_parent.sh
+++ 2.feature_extraction/cp_analysis_hpc_parent.sh
@@ -4,35 +4,34 @@
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=5:00
+#SBATCH --time=10:00
 #SBATCH --output=cp_parent-%j.out
 
 # activate cellprofiler environment
-module load anaconda
+module load miniforge
 conda init bash
 conda activate alsf_cp_env
 
 # convert all notebooks to python scripts (if any exist)
 jupyter nbconvert --to=script --FilesWriter.build_directory=nbconverted/ *.ipynb
 
-cd nbconverted/ || exit 1
+# run the LoadData CSV creation script once before submitting jobs
+python nbconverted/0.create_loaddata_csvs.py --HPC
 
 # define the round variable
 round="Round_2_data"
 
 # build the data directory path using the variable
-data_dir="../loaddata_csvs/${round}"
+data_dir="./loaddata_csvs/${round}"
 
 # get a list of all LoadData CSV files in the raw data folder
-mapfile -t loaddata_csvs < <(find "$data_dir" -type f -name "*_concatenated_with_illum.csv")
+mapfile -t loaddata_csvs < <(find "$data_dir" -type f -name "*_concatenated_with_illum.csv" | sed "s|^|$(pwd)/|")
 
 echo "Number of LoadData CSV files: ${#loaddata_csvs[@]}"
 
 for file in "${loaddata_csvs[@]}"; do
     echo "Found: $file"
 done
-
-cd ../ || exit 1
 
 # loop over each CSV and submit child jobs
 for loaddata_file in "${loaddata_csvs[@]}"; do
